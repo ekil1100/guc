167f4e6130c124d02a5d476bc30aebd96108f7f6
lastingman <keithleaves@gmail.com>
2021-02-02 15:22:52 +0800

    init

---
+++ pages/api/StreamingSE.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import got from 'got'
+
+export default async (
+  _: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const file = await got(
+    'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Clash/RuleSet/StreamingMedia/StreamingSE.yaml'
+  )
+  res.status(200).send(file.body)
+}
